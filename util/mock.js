65b14bc4333f7b3ca1741c53f94f06b7b41687ee
Max Alcala <malcala@shoppertrak.com>
2016-08-08 14:28:19 -0500

    Fix transaction test, remove leak warning

--- util/mock.js
+++ util/mock.js
@@ -1,8 +1,25 @@
-module.exports = function mockProperty(object, property, method) {
+function mockProperty(object, property, method) {
   var mockedProperty = object[property];
   object[property] = method;
 
   method.restore = function () {
     object[property] = mockedProperty;
   }
+}
+
+function mockPrototype(object, property, method) {
+  var mockedProperty = object[property];
+  var mockedPrototype = object.constructor.prototype[property];
+
+  object.constructor.prototype[property] = object[property] = method;
+
+  method.restore = function () {
+    object[property] = mockedProperty;
+    object.constructor.prototype[property] = mockedPrototype;
+  }
+}
+
+module.exports = {
+  mockProperty: mockProperty,
+  mockPrototype: mockPrototype,
 }

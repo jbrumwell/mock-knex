407fe0dc8b8b378bd862fbcc3b7d29cb4d9d022c
Jason Brumwell <jason.brumwell@gmail.com>
2018-07-18 13:48:01 -0400

    Support Version 0.15

--- .labrc.js
+++ .labrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  globals: '__BluebirdErrorTypes__,Set,Intl,Map,__core-js_shared__,System,Observable,regeneratorRuntime,asap,core,_babelPolyfill,WebAssembly',
+  globals: 'SharedArrayBuffer,Atomics,__BluebirdErrorTypes__,Set,Intl,Map,__core-js_shared__,System,Observable,regeneratorRuntime,asap,core,_babelPolyfill,WebAssembly',
   verbose: true,
 }
